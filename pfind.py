e92133b87803453ba27f761e409cb3e74fe3bb00
minninn <rn2685rn@gmail.com>
2022-11-30 16:58:13 +0900

    20221130

--- pfind.py
+++ pfind.py
@@ -1,3 +1,5 @@
+#!/opt/homebrew/bin/python3
+
 import os
 import sys
 
@@ -10,14 +12,16 @@ for child_path, dirs, files in os.walk( path ):
 
 try:
     string  = sys.argv[1]
+    cnt     = 0
     for childFile in childFiles:
-        if os.system( "cat {0} | grep {1} 1>/dev/null".format( childFile, string ) ) == 0:
+        if os.system( "cat {0} | grep -i '{1}' 1>/dev/null".format( childFile, string ) ) == 0:
+            cnt += 1
             print( "match: {0}".format( childFile ) )
-    
-    print( "Files: {0}".format( len( childFiles ) ) )
+
+    print( "Files: {0}/{1}".format( cnt, len( childFiles ) ) )
 
 except:
     for childFile in childFiles:
         print( childFile )
-    
-    print( "Files: {0}".format( len( childFiles ) ) )    
+
+    print( "Files: {0}".format( len( childFiles ) ) )
