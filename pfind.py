a53e756cfc15ee117b114c31a9a9cd49df20a4e5
guaca123 <hayeonggwag2@gmail.com>
2022-12-04 20:31:51 +0900

    1.0.0

--- pfind.py
+++ pfind.py
@@ -1,5 +1,14 @@
 #!/opt/homebrew/bin/python3
 
+# --------------------------------
+# Version: 1.0.0
+# OS: macOS Ventura 13.0.1
+# Last Modify: 2022.12.04.
+# Made by: minninn, guaca123
+# https://github.com/minninn/pfind
+# --------------------------------
+
+
 import os
 import sys
 
