19ccc56dc3e6c233d92cb68aa8117b024ee71dfe
minninn <95167298+minninn@users.noreply.github.com>
2023-04-10 14:32:54 +0900

    1.1.1

--- pfind.sh
+++ pfind.sh
@@ -1,4 +1,6 @@
-#!/bin/zsh
+#!/bin/sh
+
+python3 -m pip install tqdm
 
 USER_SHELL=`echo $SHELL | cut -c 6-`
 DIR_PATH=`pwd`
@@ -6,6 +8,11 @@ echo $USER_SHELL
 
 if [ "$USER_SHELL" == "zsh" ];then
     echo `mkdir ~/.zprofile`
-    echo `echo alias pfind='python3 $DIR_PATH/pfind.py' > .zprofile`
+    echo `echo alias pfind="'python3 $DIR_PATH/pfind.py'" > ~/.zprofile`
+    . ~/.zprofile
 fi
 
+if [ "$USER_SHELL" == "bash" ]; then
+    echo `echo alias pfind="'python3 $DIR_PATH/pfind.py'" > ~/.bashrc`
+    . ~/.bashrc
+fi
